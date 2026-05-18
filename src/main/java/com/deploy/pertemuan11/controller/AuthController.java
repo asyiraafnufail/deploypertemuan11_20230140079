51c7c367d87945c7a84e554528430f2db8a14da7
asyiraafnufail <asyiraaf4@gmail.com>
2026-05-18 12:00:22 +0700

    add controller

---
+++ src/main/java/com/deploy/pertemuan11/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.deploy.pertemuan11.controller;
+
+import com.deploy.pertemuan11.model.dto.RegisterRequest;
+import com.deploy.pertemuan11.service.AuthService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute RegisterRequest request) {
+        authService.register(request);
+        return "redirect:/login";
+    }
+}
