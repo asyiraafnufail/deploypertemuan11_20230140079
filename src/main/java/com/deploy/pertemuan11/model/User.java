3593cfe31ac2cae7c472e425636abb0d5b35d789
asyiraafnufail <asyiraaf4@gmail.com>
2026-05-18 10:51:58 +0700

    feat: membuat entitas dasar User dan field utama

--- src/main/java/com/deploy/pertemuan11/model/User.java
+++ src/main/java/com/deploy/pertemuan11/model/User.java
@@ -1,5 +1,22 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    
+    @Id
+    private String id;
+    private String username;
+    private String password;
 }
