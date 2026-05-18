04f26eab10adac06747d57008f494932fa149145
asyiraafnufail <asyiraaf4@gmail.com>
2026-05-18 10:46:47 +0700

    feat: menambahkan entity Profile dan relasi ke User

--- src/main/java/com/deploy/pertemuan11/model/Profile.java
+++ src/main/java/com/deploy/pertemuan11/model/Profile.java
@@ -1,1 +1,28 @@
 package com.deploy.pertemuan11.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    @Id
+    private String id;
+    private String nama;
+    private String alamat;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
